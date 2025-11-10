e57e045e01570ce048e260d145bc97ae8586f3d8
KereszturiHanna <kerhanna05@gmail.com>
2025-11-10 10:17:56 +0100

    Pull teszt

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <header>
            <nav
             class="nav justify-content-center">
-            <a class="nav-link active" href="#" aria-current="page">Active link</a
+            <a class="nav-link active" href="#" aria-current="page">LTC</a
             >
             <a class="nav-link" href="#">Link</a>
             <a class="nav-link disabled" href="#">Disabled link</a>
