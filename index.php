ca3a4a15faf7737d6df2dadf37b9d7a04db30b9a
KereszturiHanna <kerhanna05@gmail.com>
2025-11-10 10:14:26 +0100

    Navbar

--- index.php
+++ index.php
@@ -20,7 +20,14 @@
 
     <body>
         <header>
-            <!-- place navbar here -->
+           <nav
+            class="nav justify-content-center">
+            <a class="nav-link active" href="#" aria-current="page">Active link</a
+            >
+            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link disabled" href="#">Disabled link</a>
+           </nav>
+           
         </header> 
         <main></main>
         <footer>
